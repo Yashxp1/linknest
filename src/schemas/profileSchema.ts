1291040620b66dd52d4ae0fe3e15d229368ef090
Yash Raj <kyashraj991@gmail.com>
2025-07-13 23:26:03 +0530

    feat: implement zustand store for auth

--- src/schemas/profileSchema.ts
+++ src/schemas/profileSchema.ts
@@ -4,4 +4,5 @@ export const cardSchema = z.object({
   title: z.string().min(1, 'Title is required'),
   bio: z.string().max(160).optional(),
   slug: z.string().min(1, 'Slug is required'),
+  profilePic: z.string().optional(),
 });
---
+++ src/store/authStore.ts
@@ -0,0 +1,71 @@
+import { create } from 'zustand';
+import axios from 'axios';
+import { toast } from 'sonner';
+
+const baseURL = 'http://localhost:3000/api';
+
+type regiterData = {
+  name: string;
+  username: string;
+  password: string;
+};
+
+type signinData = {
+  username: string;
+  password: string;
+};
+
+type AuthState = {
+  isLoading: boolean;
+  signup: (data: regiterData) => Promise<boolean>;
+  signin: (data: signinData) => Promise<boolean>;
+  logout: () => Promise<void>;
+};
+
+export const useAuthStore = create<AuthState>((set, get) => ({
+  isLoading: false,
+
+  signup: async (data) => {
+    set({ isLoading: true });
+    try {
+      await axios.post(`${baseURL}/auth/signup`, data, {
+        withCredentials: true,
+      });
+      toast.success('signuped successfully');
+      return true;
+    } catch (error: any) {
+      console.error(error);
+      toast.error(error.response?.data?.message || 'Registration failed');
+      return false;
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  signin: async (data: signinData) => {
+    set({isLoading: true})
+    try {
+      const res = await axios.post(`${baseURL}/auth/signin`, data, {
+        withCredentials: true,
+      });
+      toast.success('Signed in successfully');
+      return true;
+    } catch (error: any) {
+      console.error(error);
+      toast.error(error.response.data.message || 'Login failed');
+      return false;
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axios.post(`${baseURL}/api/logout`, {}, { withCredentials: true });
+      toast.success('Logged out');
+    } catch (error: any) {
+      console.error(error);
+      toast.error(error.response.data.message || 'Logout failed');
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+}));
