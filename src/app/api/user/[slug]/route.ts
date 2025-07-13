2707d69d1dd12e5c2304e6ab970eea8e135b90aa
Yash Raj <kyashraj991@gmail.com>
2025-07-13 12:02:50 +0530

    feat: add sharable link logic

---
+++ src/app/api/user/[slug]/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  req: NextResponse,
+  { params }: { params: { slug: string } }
+) {
+  try {
+    const { slug } = params;
+
+    const profile = await prisma.profile.findUnique({
+      where: {
+        slug,
+      },
+      include: {
+        links: true,
+      },
+    });
+
+    if (!profile) {
+      return NextResponse.json(
+        { messgae: 'Profile not found' },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        profile,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error: any) {
+    console.error('Slug error');
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  }
+}
