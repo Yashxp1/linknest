3eef44eefd691f6f4080c8609a20ba8ecaf7634f
Yash Raj <kyashraj991@gmail.com>
2025-07-13 23:26:17 +0530

    feat: protect routes using middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,29 @@
-import { verify } from 'crypto';
 import { NextRequest, NextResponse } from 'next/server';
-import { verifyToken } from './lib/auth';
+import { verifyToken } from '@/lib/auth';
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('session')?.value;
-  const isApiRoute = req.url.startsWith('/api');
+  const pathname = req.nextUrl.pathname;
 
-  if (!token) {
-    if (isApiRoute) {
-      return NextResponse.json(
-        {
-          message: 'Authetication require',
-        },
-        { status: 401 }
-      );
-    }
-    return NextResponse.redirect(new URL('/login', req.url));
+  const isProtectedRoute = ['/dashboard'].some((route) =>
+    pathname.startsWith(route)
+  );
+
+  const isAuthRoute = ['/signin', '/signup'].includes(pathname);
+
+  const decoded = token ? verifyToken(token) : null;
+
+  if (isProtectedRoute && !decoded) {
+    return NextResponse.redirect(new URL('/signin', req.url));
   }
 
-  const decode = verifyToken(token);
-
-  if (!decode) {
-    const response = isApiRoute
-      ? NextResponse.json({ message: 'Invalid token' }, { status: 401 })
-      : NextResponse.redirect(new URL('/login', req.url));
-    response.cookies.delete('session');
-    return response;
+  if (isAuthRoute && decoded) {
+    return NextResponse.redirect(new URL('/dashboard', req.url));
   }
 
   return NextResponse.next();
 }
+
 export const config = {
-  matcher: [
-    '/home/:path*',
-    // '/dashboard/:path*',
-    // '/profile/:path*',
-    // '/settings/:path*',
-  ],
+  matcher: ['/dashboard/:path*', '/sigin', '/signup'],
 };
