3c207db6c32ef13d234cd24865658385c6c332d3
Yash Raj <kyashraj991@gmail.com>
2025-07-15 18:40:54 +0530

    feat: implement next-auth config

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,15 @@
-import NextAuth from "next-auth"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from './lib/prisma';
+import authConfig from './auth.config';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: 'jwt' },
+  ...authConfig
+});
