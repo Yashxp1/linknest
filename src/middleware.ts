b9d3e76207ce16137fc9c5052ad767e2ae8484a3
Yash Raj <kyashraj991@gmail.com>
2025-07-16 22:55:44 +0530

    feat: implement middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,39 @@
-export { auth as middleware } from "@/auth"
+
+import NextAuth from 'next-auth';
+
+import authConfig from './auth.config';
+import { privateRoutes } from './routes';
+
+const { auth } = NextAuth(authConfig);
+
+export default auth(async (req) => {
+  // console.log('Middleware called', req.nextUrl.pathname);
+  // console.log(req.auth);
+
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+
+  const url = 'http://localhost:3000';
+
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = nextUrl.pathname.includes('/auth');
+  const isApiRoute = nextUrl.pathname.includes('/api');
+
+  if (isApiRoute) return;
+
+  if (isLoggedIn && isAuthRoute) {
+    return Response.redirect(`${url}/dashboard`);
+  }
+
+  if (isAuthRoute && !isLoggedIn) {
+    return;
+  }
+
+  if (!isLoggedIn && isPrivateRoute) {
+    return Response.redirect(`${url}/auth/login`);
+  }
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
