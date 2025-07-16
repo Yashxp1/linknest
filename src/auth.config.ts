c601c1bc56f79bd9aa3309ce62045d5c4d57c010
Yash Raj <kyashraj991@gmail.com>
2025-07-16 22:18:30 +0530

    feat: add login config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,36 @@
 import Google from 'next-auth/providers/google';
 import Credentials from 'next-auth/providers/credentials';
 import type { NextAuthConfig } from 'next-auth';
+import { LoginSchema } from './schemas/authSchema';
+import bcrypt from 'bcryptjs';
+import { prisma } from './lib/prisma';
 
 export default {
-  providers: [Google({}), Credentials({})],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
+    Credentials({
+      async authorize(credentials) {
+        const validateData = LoginSchema.safeParse(credentials);
+        if (!validateData.success) return null;
+        const { email, password } = validateData.data;
+        const user = await prisma.user.findFirst({
+          where: {
+            email: email,
+          },
+        });
+        if (!user || !user.password || !user.password) {
+          return null;
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) return user;
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
