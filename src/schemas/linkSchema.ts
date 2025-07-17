a02f7ada97cbfde84a1583702f371bdae2f13f00
Yash Raj <kyashraj991@gmail.com>
2025-07-17 23:14:46 +0530

    feat: add link schema

--- src/schemas/linkSchema.ts
+++ src/schemas/linkSchema.ts
@@ -10,5 +10,5 @@ export const linkSchema = z.object({
     .string()
     .min(1, 'URL is required')
     .regex(urlRegex, 'Invalid URL format'),
-    profileId: z.string().cuid('Invalid Profile ID'),
+    // profileId: z.string().cuid('Invalid Profile ID'),
 });
