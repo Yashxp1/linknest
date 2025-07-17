f62c60717c56a4ba17734cb93a0018643a7fe7e0
Yash Raj <kyashraj991@gmail.com>
2025-07-17 23:13:31 +0530

    fix: refacet link endpoint

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -11,22 +11,44 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
 
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+      include: { profiles: true },
+    });
+
+    if (!user || !user.profiles || user.profiles.length === 0) {
+      return NextResponse.json(
+        { message: 'Profile not found' },
+        { status: 404 }
+      );
+    }
+
+    const profile = user.profiles[0];
+
     const body = await req.json();
-    const validatedData = linkSchema.parse(body);
+    const result = linkSchema.safeParse(body);
+
+    if (!result.success) {
+      return NextResponse.json({ message: 'Invalid input' }, { status: 400 });
+    }
+
+    const validatedData = result.data;
 
     const latestOrder = await prisma.link.count({
-      where: { profileId: validatedData.profileId },
+      where: { profileId: profile.id },
     });
 
     const link = await prisma.link.create({
       data: {
         title: validatedData.title,
         url: validatedData.url,
-        profileId: validatedData.profileId,
+        profileId: profile.id,
         order: latestOrder + 1,
       },
     });
-
+    console.log(link)
     return NextResponse.json({ success: true, link }, { status: 200 });
   } catch (error: any) {
     console.error(error);
