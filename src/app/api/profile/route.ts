f55bf929be8b0387b2f6b696c1be8e96ffb15d87
Yash Raj <kyashraj991@gmail.com>
2025-07-19 10:56:12 +0530

    fix: put slug on hold

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,62 +1,61 @@
-import { auth } from '@/auth';
-import { prisma } from '@/lib/prisma';
-import { profileSchema } from '@/schemas/profileSchema';
-import { NextRequest, NextResponse } from 'next/server';
+// import { auth } from '@/auth';
+// import { prisma } from '@/lib/prisma';
+// import { profileSchema } from '@/schemas/profileSchema';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
+// export async function POST(req: NextRequest) {
+//   try {
+//     const body = await req.json();
 
-    const session = await auth();
+//     const session = await auth();
 
-    if (!session?.user?.id || !session.user.email) {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    }
+//     if (!session?.user?.id || !session.user.email) {
+//       return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+//     }
 
-    const validatedData = profileSchema.parse(body);
+//     const validatedData = profileSchema.parse(body);
 
-    // const slug =
-    //   validatedData.slug ??
-    //   validatedData.title.toLowerCase().replace(/\s+/g, '-');
+//     // const slug =
+//     //   validatedData.slug ??
+//     //   validatedData.title.toLowerCase().replace(/\s+/g, '-');
 
-    const profile = await prisma.profile.create({
-      data: {
-        userId: session.user.id,
-        title: validatedData.title,
-        location: validatedData.location,
-        bio: validatedData.bio,
-        profilePic: validatedData.profilePic,
-        slug: validatedData.slug,
-      },
-    });
+//     const profile = await prisma.profile.create({
+//       data: {
+//         userId: session.user.id,
+//         location: validatedData.location,
+//         bio: validatedData.bio,
+//         profilePic: validatedData.profilePic,
+//         slug: validatedData.slug,
+//       },
+//     });
 
-    // console.log('PROFILE SESSION ID: ', session.user.id);
+//     // console.log('PROFILE SESSION ID: ', session.user.id);
 
-    return NextResponse.json({ success: true, profile }, { status: 201 });
-  } catch (error: any) {
-    console.error('Error profile -->', error.message);
-    return NextResponse.json({ message: 'Server error' }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ success: true, profile }, { status: 201 });
+//   } catch (error: any) {
+//     console.error('Error profile -->', error.message);
+//     return NextResponse.json({ message: 'Server error' }, { status: 500 });
+//   }
+// }
 
-export async function GET(req: NextRequest) {
-  try {
-    const session = await auth();
+// export async function GET(req: NextRequest) {
+//   try {
+//     const session = await auth();
 
-    if (!session?.user?.id || !session.user.email) {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    }
+//     if (!session?.user?.id || !session.user.email) {
+//       return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+//     }
 
-    const ProfileData = await prisma.profile.findMany({
-      where: { userId: session.user.id },
-      include: {
-        link: true,
-      },
-    });
+//     const ProfileData = await prisma.profile.findMany({
+//       where: { userId: session.user.id },
+//       include: {
+//         link: true,
+//       },
+//     });
 
-    return NextResponse.json({ ProfileData }, { status: 200 });
-  } catch (error: any) {
-    console.error('Error in profile --->', error.message);
-    return NextResponse.json({ message: 'Server error' }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ ProfileData }, { status: 200 });
+//   } catch (error: any) {
+//     console.error('Error in profile --->', error.message);
+//     return NextResponse.json({ message: 'Server error' }, { status: 500 });
+//   }
+// }
