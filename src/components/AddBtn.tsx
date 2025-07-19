5bfa21c53207f9e9111b6d1bfda8ab02d35cdfa1
Yash Raj <kyashraj991@gmail.com>
2025-07-19 12:40:18 +0530

    feat: add default profile icon

--- src/components/AddBtn.tsx
+++ src/components/AddBtn.tsx
@@ -1,12 +1,10 @@
 'use client';
 import React from 'react';
 import { Button } from './ui/button';
-import { Divide, Plus } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { Modal } from './Modal';
-import { auth } from '@/auth';
-
+import Image from 'next/image';
 const AddBtn = () => {
-
   return (
     <Modal
       trigger={(openModal) => (
@@ -14,15 +12,21 @@ const AddBtn = () => {
           <div className="w-[80%]">
             <div className="w-full p-4 pb-6 flex flex-col">
               <div>
-                <div className="flex  py-4">
-                  <img
-                    src="ss.png"
-                    alt="pfp"
-                    className="w-18 h-18 object-cover rounded-full border"
+                <div className="flex items-center space-x-3 py-4">
+                  <Image
+                    src="/default1.jpg"
+                    alt="Profile Picture"
+                    width={64}
+                    height={64}
+                    className="rounded-full object-cover border"
                   />
-                  <div className="px-4 flex flex-col">
-                    <span className="font-semibold">Yashxp1</span>
-                    <p className="text-md">Full-Stack developer</p>
+                  <div className="flex flex-col gap-0.5 leading-tight">
+                    <span className="font-semibold">
+                      Yashxp1
+                    </span>
+                    <p className="text-md">
+                      Full-Stack Developer
+                    </p>
                     <p className="text-sm">Ranchi, India</p>
                   </div>
                 </div>
