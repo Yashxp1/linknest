16d69950e41902d3d76e510e3c6da96ff3e9cb8e
Yash Raj <kyashraj991@gmail.com>
2025-07-19 10:56:41 +0530

    fix: userId fix

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,6 +1,7 @@
 import { auth } from '@/auth';
 import { prisma } from '@/lib/prisma';
 import { linkSchema } from '@/schemas/linkSchema';
+import { deleteSchema, linkUpdateSchema } from '@/schemas/linkSchema';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
@@ -15,19 +16,13 @@ export async function POST(req: NextRequest) {
       where: {
         email: session.user.email,
       },
-      include: { profiles: true },
     });
 
-    if (!user || !user.profiles || user.profiles.length === 0) {
-      return NextResponse.json(
-        { message: 'Profile not found' },
-        { status: 404 }
-      );
+    if (!user?.id) {
+      return NextResponse.json({ message: 'Unathorized' }, { status: 401 });
     }
 
-    const profile = user.profiles[0];
-
-    const body = await req.json();
+     const body = await req.json();
     const result = linkSchema.safeParse(body);
 
     if (!result.success) {
@@ -37,18 +32,27 @@ export async function POST(req: NextRequest) {
     const validatedData = result.data;
 
     const latestOrder = await prisma.link.count({
-      where: { profileId: profile.id },
+      where: { userId: user?.id },
     });
+
+    // const user = await prisma.user.findUnique({where: {
+    //   id: user?.id
+    // }})
+
+    // if(!user) {
+    //   return NextResponse.json({})
+    // }
 
     const link = await prisma.link.create({
       data: {
         title: validatedData.title,
         url: validatedData.url,
-        profileId: profile.id,
+        userId: user.id,
+        // userId: validatedData.userId,
         order: latestOrder + 1,
       },
     });
-    // console.log(link)
+    console.log(link);
     return NextResponse.json({ success: true, link }, { status: 200 });
   } catch (error: any) {
     console.error(error);
@@ -60,6 +64,130 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    return NextResponse.json({ success: 'Server Error' }, { status: 500 });
+    return NextResponse.json(
+      { success: false, message: 'Server Error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await auth();
+
+    if (!session || !session.user || !session.user.email) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!user?.id) {
+      return NextResponse.json({ message: 'Unathorized' }, { status: 401 });
+    }
+
+    const links = await prisma.link.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    if (!links) {
+      return NextResponse.json({ message: 'Links not found' }, { status: 404 });
+    }
+    return NextResponse.json({ success: true, res: links }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, message: 'Server Error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const session = await auth();
+
+    if (!session || !session.user || !session.user.email) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!user?.id) {
+      return NextResponse.json({ message: 'Unathorized' }, { status: 401 });
+    }
+
+    const body = await req.json();
+    const result = linkUpdateSchema.safeParse(body);
+
+    if (!result.success) {
+      return NextResponse.json({ message: 'Invalid input' }, { status: 400 });
+    }
+
+    const validatedData = result.data;
+
+    const updatedLink = await prisma.link.update({
+      where: { id: validatedData.linkId, userId: user.id },
+      data: {
+        title: validatedData.title,
+        url: validatedData.url,
+      },
+    });
+
+    return NextResponse.json(
+      { success: 'true', res: updatedLink },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: 'Server Error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const session = await auth();
+
+    if (!session || !session.user || !session.user.email) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!user?.id) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const { linkId } = deleteSchema.parse(await req.json());
+
+    const deletedLink = await prisma.link.delete({
+      where: {
+        id: linkId,
+        userId: user.id,
+      },
+    });
+
+    return NextResponse.json({ success: true, deletedLink });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, message: 'Server Error' },
+      { status: 500 }
+    );
   }
 }
