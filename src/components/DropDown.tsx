0964c00b9f42a07c26b1afed05722d7b107d1c30
Yash Raj <kyashraj991@gmail.com>
2025-07-19 12:40:50 +0530

    feat: add delete link logic

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,13 +1,40 @@
+'use client';
+import { auth } from '@/auth';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-
+import { userLinkStore } from '@/store/linkStore';
 import { Plus } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
-const DropDown = () => {
+interface DropDownProps {
+  linkId: string;
+  userId: string;
+}
+
+const DropDown = ({ linkId, userId }: DropDownProps) => {
+  const { isLoading, deleteLink, getLink } = userLinkStore();
+  // const [isOpen, setIsOpen] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      const res = await deleteLink({ linkId: linkId });
+      if (res.success) {
+        await getLink();
+        //  setIsOpen(false)
+      }
+    } catch (error) {
+      console.error('Failed to delete link:', error);
+    }
+  };
+
+  // useEffect(() => {
+  //   deleteLink(id);
+  // }, [deleteLink]);
+
   return (
     <div className="  ">
       <DropdownMenu>
@@ -18,7 +45,9 @@ const DropDown = () => {
           <DropdownMenuItem variant="destructive"> Cancel</DropdownMenuItem>
 
           <DropdownMenuItem>Edit</DropdownMenuItem>
-          <DropdownMenuItem>Delete</DropdownMenuItem>
+          <DropdownMenuItem onClick={handleDelete} disabled={isLoading}>
+            {isLoading ? 'Deleting...' : 'Delete'}
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
