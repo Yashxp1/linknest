de94bbef104e0a7fcadc8a080c85a796ec9424df
Yash Raj <kyashraj991@gmail.com>
2025-07-22 23:18:51 +0530

    update: prisma and profile schema fields

--- src/schemas/profileSchema.ts
+++ src/schemas/profileSchema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const profileSchema = z.object({
   bio: z.string().max(160),
   location: z.string().max(30).optional(),
-  slug: z.string().min(1, 'Slug is required'),
-  profilePic: z.string().optional(),
+  // slug: z.string().min(1, 'Slug is required'),
+  image: z.string().optional(),
 });
 
