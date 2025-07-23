8db0b8cefe9c0f192ebdfcb00d8e0ae5f3724d89
Yash Raj <kyashraj991@gmail.com>
2025-07-23 14:06:47 +0530

    feat: create slug on credential based auth

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -3,6 +3,8 @@ import bcrypt from 'bcryptjs';
 import { prisma } from '@/lib/prisma';
 import { RegisterSchema } from '@/schemas/authSchema';
 import * as z from 'zod';
+import { generateSlug } from '@/lib/GenerateSlug';
+import { NextResponse } from 'next/server';
 
 export const register = async (data: z.infer<typeof RegisterSchema>) => {
   try {
@@ -31,13 +33,22 @@ export const register = async (data: z.infer<typeof RegisterSchema>) => {
 
     const lowercaseEmail = email.toLowerCase();
 
+    const slug = generateSlug(name || email);
+
+    if (!slug) {
+      return NextResponse.json({ message: 'Slug not found' }, { status: 404 });
+    }
+
     const user = await prisma.user.create({
       data: {
         email: lowercaseEmail,
         name,
         password: hashPassword,
+        slug,
       },
     });
+
+    // const slug = generateSlug(user.id);
 
     return { success: `Email verification is sent on ${lowercaseEmail}` };
   } catch (error) {
