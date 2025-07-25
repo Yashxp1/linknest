f11ea6653e1fa7fe2d1d1a7310818d8415b68147
Yash Raj <kyashraj991@gmail.com>
2025-07-25 09:24:20 +0530

    feat: wrap LinkPage component with SessionProvider for session management

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import LinkPage from '@/components/LinkPage/Dashboard';
 import Navbar from '@/components/Navbar';
+import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 
 const page = () => {
   return (
     <div className="">
-      <LinkPage />
+      <SessionProvider>
+        <LinkPage />
+      </SessionProvider>
     </div>
   );
 };
