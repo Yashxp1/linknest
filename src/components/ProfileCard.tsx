bf20872baf060711d6d7f49d0e6ece70aad707e6
Yash Raj <kyashraj991@gmail.com>
2025-07-26 20:48:02 +0530

    feat: get slug in FE

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -5,6 +5,7 @@ import { Mona_Sans } from 'next/font/google';
 import { userProfileStore } from '@/store/profileStore';
 import { useSession } from 'next-auth/react';
 import { userLinkStore } from '@/store/linkStore';
+import { useParams } from 'next/navigation';
 
 const MonaSansfont = Mona_Sans({
   subsets: ['latin'],
@@ -14,6 +15,11 @@ const MonaSansfont = Mona_Sans({
 const ProfileCard = () => {
   const { data: session } = useSession();
 
+  const params = useParams();
+  const slug = 'yash-raj-V0SKS5qMDmLoWD4GrCPSA';
+
+  console.log(slug);
+
   const profile = userProfileStore((state) => state.profile);
   const getProfile = userProfileStore((state) => state.getProfile);
 
@@ -21,10 +27,22 @@ const ProfileCard = () => {
   const visibleLinks = userLinkStore((state) => state.visibleLinks);
   const getVisibleLink = userLinkStore((state) => state.getVisibleLink);
 
+  // const getLink = userLinkStore((state) => state.getLink);
 
   useEffect(() => {
-    getVisibleLink();
-  }, []);
+    if (slug && !profile) {
+      getProfile(slug);
+      getVisibleLink();
+    }
+  }, [slug, profile]);
+
+  // useEffect(() => {
+  //   getVisibleLink();
+  // }, []);
+
+  // useEffect(() => {
+  //   getLink();
+  // }, [getLink]);
 
   useEffect(() => {
     if (session?.user?.slug && !profile) {
@@ -33,11 +51,14 @@ const ProfileCard = () => {
   }, [session?.user?.slug, profile]);
 
   return (
-    <div className={`${MonaSansfont.className} pt-13`}>
-      <div className="flex flex-col items-center justify-center px-2 pt-2 pb-4 rounded-4xl bg-gradient-to-br from-pink-300 via-white to-blue-100 shadow-sm shadow-purple-300 dark:shadow-none transition-all duration-300 backdrop-blur-sm">
+    <div
+      className={`flex items-center justify-center h-screen ${MonaSansfont.className}`}
+    >
+      <div className="flex flex-col items-center justify-center px-4 pt-4 pb-6 rounded-4xl bg-gradient-to-br from-pink-300 via-white to-blue-100 shadow-sm shadow-purple-300 dark:shadow-none transition-all duration-300 backdrop-blur-sm">
+      
         <div className="relative w-[21rem] aspect-square overflow-hidden rounded-4xl group">
           <Image
-            src={profile?.image || '/goku.jpg'}
+            src={profile?.image || '/looney.jpg'}
             alt="User Image"
             fill
             className="object-cover transition-transform duration-500 group-hover:scale-110"
@@ -45,10 +66,10 @@ const ProfileCard = () => {
           <div className="absolute inset-0 bg-gradient-to-t from-black/20 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
         </div>
 
-        <div className="w-[21rem] mt-2">
+
+        <div className="w-[21rem] mt-3">
           <div className="pl-5">
             <h2 className="text-2xl font-bold text-transparent bg-gradient-to-r from-gray-800 to-gray-600 bg-clip-text hover:from-blue-600 hover:to-purple-600 transition-all duration-300">
-              {/* {profile?.name || 'Your Name'} */}
               Yashxp1
             </h2>
             <p className="text-gray-600 text-sm hover:text-gray-800 transition-colors duration-200">
@@ -75,7 +96,7 @@ const ProfileCard = () => {
                   href={link.url}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="border border-gray-300 w-full py-2 rounded-xl cursor-pointer transition-all bg-white"
+                  className="border border-gray-300 w-full py-2 rounded-xl cursor-pointer transition-all bg-white hover:bg-gray-100"
                 >
                   {link.title}
                 </a>
