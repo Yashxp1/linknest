a9dd7dce10e9612e861ce7ec8ac3038b3b99356e
Yash Raj <kyashraj991@gmail.com>
2025-07-29 19:39:32 +0530

    feat: setup axios to generate shorten url

---
+++ src/store/shortener.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { toast } from 'sonner';
+
+interface shortenResponse {
+  data: {
+    id: string;
+    slug: string;
+    url: string;
+    createdAt: string;
+  };
+  shortURL: string;
+}
+
+export const shortener = async (
+  url: string
+): Promise<shortenResponse | null> => {
+  try {
+    const res = await axios.post<shortenResponse>(
+      `http://localhost:3000/api/shorten`,
+      { url }
+    );
+    console.log(res.data);
+    toast.success('URL shortened successfully');
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    toast.error('failed to shorten url');
+    return null;
+  }
+};
