803865794bcaabd64c9cc3873c96bd923fa741b2
Yash Raj <kyashraj991@gmail.com>
2025-07-30 19:55:29 +0530

    feat: implement reorder logic in backend

--- src/app/api/links/reorder/route.ts
+++ src/app/api/links/reorder/route.ts
@@ -6,36 +6,35 @@ export async function PUT(req: NextRequest) {
   try {
     const session = await auth();
 
-    if (!session || !session.user?.email) {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 500 });
-    }
-
-    const user = await prisma.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
-
-    if (!user?.id) {
+    if (!session || !session.user || !session.user.id) {
       return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
 
-    // const {orderedLinkIds} = req.json()
+    const body = await req.json();
 
-    const { orderedLinkIds } = await req.json();
+    const { orderedLinkIds } = body;
+
+    if (!Array.isArray(orderedLinkIds)) {
+      return NextResponse.json({ message: 'Invalid input' }, { status: 400 });
+    }
 
     await Promise.all(
-      orderedLinkIds.map((id: string, index: number) =>
+      orderedLinkIds.map((id, index) =>
         prisma.link.update({
-          where: { id, userId: user.id },
-          data: { order: index },
+          where: {
+            id,
+            userId: session.user.id,
+          },
+          data: {
+            order: index,
+          },
         })
       )
     );
 
-    return NextResponse.json({ success: true }, { status: 500 });
+    return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
-    console.error('Drag and drop error', error);
+    console.error('Drag and drop error:', error);
     return NextResponse.json({ success: false }, { status: 500 });
   }
 }
