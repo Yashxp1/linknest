07587da260ec6059dbb3defafaa6b4e4e3ccc093
Yash Raj <kyashraj991@gmail.com>
2025-08-02 12:27:22 +0530

    feat: add nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], 
+    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.example.com',
+        pathname: '/account123/**',
+      },
+    ],
   },
 };
 
